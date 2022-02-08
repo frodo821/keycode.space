59ffaabb30fd56c1cf0f732db7af0b45c5ea8cab
chihiro sakai <sakaic2003@gmail.com>
2022-02-09 01:26:33 +0900

    最低限の機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useLayoutEffect } from 'react';
 import './App.css';
+import EventViewer from './components/EventViewer';
 
-function App() {
+const App: React.FC = () => {
+  const [event, setter] = React.useState<KeyboardEvent | null>(null);
+
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      setter(e);
+      e.preventDefault();
+    }
+    window.addEventListener('keydown', listener);
+  }, [null]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <EventViewer event={event} />
+      </main>
+      <footer></footer>
     </div>
   );
 }
---
+++ src/components/EventViewer.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Tooltip } from '@mui/material';
+import './EventViewer.css';
+
+interface Props {
+  event: KeyboardEvent | null;
+}
+
+const EventViewer: React.FC<Props> = ({ event }) => {
+  if (event === null) {
+    return <div className="viewer no-events">Press any key to get JavaScript keycode</div>;
+  }
+
+  return (
+    <div className="viewer">
+      <div className="key-event">
+        {event.code} (<Tooltip title={<span>
+          <code className="inline">KeyboardEvent.keyCode</code> is now deprecated.
+        </span>}><span>{event.keyCode}</span></Tooltip>)
+      </div>
+      <ul className="event-details">
+        <li>
+          <Tooltip title="the key name connected with the event"><div className="name">event.key</div></Tooltip>
+          <div className="value">{event.key}</div>
+        </li>
+        <li>
+          <Tooltip title={<>
+            <code className='inline'>KeyboardEvent.code</code>
+            replaces
+            <code className='inline'>KeyboardEvent.keyCode</code>
+          </>}><div className="name">event.code</div></Tooltip>
+          <div className="value">{event.code}</div>
+        </li>
+        <li>
+          <Tooltip title="key location on the keyboard"><div className="name">event.location</div></Tooltip>
+          <div className="value">{event.location}</div>
+        </li>
+        <li>
+          <Tooltip title={<>
+            <code className='inline'>event.which</code> is now deprecated.
+            Use <code className='inline'>event.code</code> instead.
+          </>}><div className="name">event.which</div></Tooltip>
+          <div className="value">{event.which}</div>
+        </li>
+      </ul>
+      <ul className="event-modifiers">
+        <li className={`key ${event.shiftKey ? 'hold' : 'released'}`}>Shift</li>
+        <li className={`key ${event.ctrlKey ? 'hold' : 'released'}`}>Ctrl</li>
+        <li className={`key ${event.altKey ? 'hold' : 'released'}`}>Alt</li>
+        <li className={`key ${event.metaKey ? 'hold' : 'released'}`}>Meta</li>
+      </ul>
+    </div>
+  );
+}
+
+export default EventViewer;
